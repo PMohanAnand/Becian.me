2c63c839dd1c6cfb1b14f879bdbf8fac64eb335b
pvscharan <pvsaicharan111@gmail.com>
2014-09-07 21:31:05 +0530

    Revert "Revert "e-mail forwarding service""
    This reverts commit 99a506ae6d2e510fb453429d1cd759ab7e780486.

---
+++ connect.php
@@ -0,0 +1,34 @@
+<?php
+$connect = mysqli_connect("localhost","root","","charan") or die("Error " . mysqli_error($connect));
+
+$name=mysqli_real_escape_string($connect,$_POST['username']);
+$email=mysqli_real_escape_string($connect,$_POST['req_email']);
+$regno=mysqli_real_escape_string($connect,$_POST['regno']);
+$facebook=mysqli_real_escape_string($connect,$_POST['facebook']);
+$linkedin=mysqli_real_escape_string($connect,$_POST['linkedin']);
+$reqmail=mysqli_real_escape_string($connect,$_POST['reqmail']);
+
+
+
+
+if($connect)
+{
+	
+
+
+ 
+mysqli_query($connect,"INSERT INTO bec(username,email, facebook, linkedin,reqmail,regno)
+ VALUES ('$name', '$email', '$facebook', '$linkedin','$reqmail','$regno')") or die("Error " . mysqli_error($connect));
+mysqli_close($connect);
+header("Location: http://128.199.246.37/becian");
+}
+else
+{
+#just for trails purpose ,, here we have to redirect the page to the home and we have to make a alert by using javascript	
+echo "insert broke ";
+
+
+
+}
+
+?>
